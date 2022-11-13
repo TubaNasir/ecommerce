41c420a7a4db7ed4fd2760ea5f421c550c8a1e51
Faakeha Ahmed <faakehaahmed123@gmail.com>
2022-11-13 15:31:37 +0500

    notifications

---
+++ lib/notifications/notification_model.dart
@@ -0,0 +1,19 @@
+import 'dart:html';
+
+class Notification {
+  final String title;
+  final DateTime time;
+
+  Notification({
+    required this.title,
+    required this.time,
+  });
+}
+
+List<Notification> notificationList = [
+  Notification(title: "Your order abc is on it's way!", time: DateTime.now()),
+  Notification(title: "Your order abc is on it's way!", time: DateTime.now()),
+  Notification(title: "Your order abc is on it's way!", time: DateTime.now()),
+  Notification(title: "Your order abc is on it's way!", time: DateTime.now())
+
+];
